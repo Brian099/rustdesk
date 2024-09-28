af3d78995bd18f40c97dec1a9f9f68fe856fe6cf
Brian099 <80375397+Brian099@users.noreply.github.com>
2024-09-28 08:28:38 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["28918185.xyz:21116"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["28918185.xyz"];
 pub const PUBLIC_RS_PUB_KEY: &str = "MS5op+GZbc4gm1BwrJ23krFAO1JvE4efIVU7qtOmH18=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
